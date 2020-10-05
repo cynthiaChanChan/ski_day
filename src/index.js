b7bbfe61874f618e7681e5901f770894fdd4f55b
cynthiaChanChan <mession01@gmail.com>
2020-10-05 12:35:22 +0800

    Create a consoleMessages middleware

--- src/index.js
+++ src/index.js
@@ -1,21 +1,35 @@
 import C from "./constants";
-import appReducer from "./store/reducers";
-import { createStore } from "redux";
+import storeFactory from "./store";
 
-const store = createStore(appReducer);
+const initialState = localStorage["redux-store"]
+    ? JSON.parse(localStorage["redux-store"])
+    : {};
 
-const unsubscribeGoalLogger = store.subscribe(() =>
-    console.log(` Goal: ${store.getState().goal}`)
-);
+const saveState = () => {
+    const state = JSON.stringify(store.getState());
+    localStorage["redux-store"] = state;
+};
 
-setInterval(() => {
-    store.dispatch({
-        type: C.SET_GOAL,
-        payload: Math.floor(Math.random() * 100),
-    });
-}, 250);
+const store = storeFactory(initialState);
 
-setTimeout(() => {
-    //Unsubscribe from the store
-    unsubscribeGoalLogger();
-}, 3000);
+store.subscribe(saveState);
+
+store.dispatch({
+    type: C.ADD_DAY,
+    payload: {
+        resort: "Heavenly",
+        date: "2020-8-14",
+        powder: true,
+        backcountry: false,
+    },
+});
+
+store.dispatch({
+    type: C.ADD_DAY,
+    payload: {
+        resort: "Hawaii",
+        date: "2020-8-13",
+        powder: true,
+        backcountry: false,
+    },
+});
