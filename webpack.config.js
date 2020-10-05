7b5c1e2ad3816e54ca0ec69421c0130c971a3cf7
cynthiaChanChan <mession01@gmail.com>
2020-10-05 11:05:36 +0800

    Create a static build with webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "./dist/assets"),
+        filename: "bundle.js",
+        publicPath: "assets",
+    },
+    devServer: {
+        inline: true,
+        contentBase: "./dist",
+        port: 3000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+
+                exclude: /(node_modules)/,
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["@babel/preset-env"],
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+};
